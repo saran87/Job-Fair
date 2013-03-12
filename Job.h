5d8cbeecfa772927032193c43ae4aae351ca9f26
Saravana Kumar <saran87@gmail.com>
2013-03-11 22:13:50 -0400

    organized code by grouping into model and controller`

---
+++ Job.h
@@ -0,0 +1,13 @@
+//
+//  Job.h
+//  iosChallenge
+//
+//  Created by Saravana Kumar on 3/11/13.
+//  Copyright (c) 2013 Roshan Balaji Nindrai SenthilNathan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Job : NSObject
+
+@end
